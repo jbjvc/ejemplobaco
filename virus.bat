06ca6c8af86742884275a74549af00d987f0d01c
jbjvc <80050934+jbjvc@users.noreply.github.com>
2021-03-04 08:48:33 -0600

    Add files via upload
    Added a file for a prank virus

---
+++ virus.bat
@@ -0,0 +1,53 @@
+@echo off
+title holis :3 espero te guste este programa jsjsjs
+for /L %%x in (1,1,3) do (
+
+start https://i.redd.it/vr1x0betsbl51.jpg
+start https://i.chzbgr.com/full/9116434176/h8D59EFEE/meme-duck-i-cant-believe-you-6ot-plastic-surgery
+start https://i.pinimg.com/736x/4b/ea/6c/4bea6c4c530727b9fe64bf047a3229d4.jpg
+start https://ih1.redbubble.net/image.1152327417.9260/mwo,x540,ipad_2_snap-pad,600x600,f8f8f8.jpg
+start http://images2.memedroid.com/images/UPLOADED100/53eb532236c9f.jpeg
+start http://www.tnrelaciones.com/informacion/wp-content/uploads/2014/02/animales-32.jpg
+start https://pbs.twimg.com/media/EXQYHTpWkAA4DUi.jpg
+start https://www.chistesparaniños.es/wp-content/uploads/2020/11/patoalavida.jpg
+start https://i.pinimg.com/originals/4f/6a/4b/4f6a4b0fd956d589a294d102d28cd95a.jpg
+
+start winword
+start mspaint
+start notepad
+start write
+start cmd
+start explorer
+start control
+start calc
+
+
+md jajaj
+cd jajaj
+md jajaja
+cd jajaja
+cd..
+md khe sta pazanda
+md abreestacarpeta:3
+cd abreestacarpeta:3
+md quecreisi
+md pasa 
+md io no jui
+cd..
+md elige una carpeta jsjs
+cd elige una carpeta jsjs
+md en esta hay una sorpresa
+md o quiza nO
+
+)
+
+color 04x
+:hackintento
+echo e5t45 5iend0 h4ck34d0 tu c0mput4d0r s3 ap4g4g4ra en c0rto
+goto hackintento
+
+
+
+pause
+
+exit
